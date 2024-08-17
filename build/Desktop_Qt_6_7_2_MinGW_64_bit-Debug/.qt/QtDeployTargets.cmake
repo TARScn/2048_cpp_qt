c916ae5143e5207b6ec924953847c9093ad21424
StarButterfly <luocx0301@gmail.com>
2024-08-18 00:02:37 +0800

    Delete build directory

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_samp_3_1_FILE E:/vscode_ws/QT_WS/samp_3_1/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/samp_3_1.exe)
-set(__QT_DEPLOY_TARGET_samp_3_1_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_samp_3_1_RUNTIME_DLLS E:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;E:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;E:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
